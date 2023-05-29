206780c037497c04a6786965e82fc141e0f56dde
zheaEvyline <99989764+zheaEvyline@users.noreply.github.com>
2023-05-29 13:34:54 +0000

    Deploying to generated from @ BedrockCommands/bedrockcommands.github.io@4accb4f89d9900cfa2dfe71fbbab8be10c6b8b2b 🚀

--- assets/app.45e7af37.js
+++
@@ -1,1 +0,0 @@
-import{t as r}from"./404.md.4d538e70.js";export{r as createApp};
---
+++ assets/commands_block-entities.md.1940a4f6.lean.js
@@ -0,0 +1,56 @@
+import{_ as c}from"./chunks/6.07d6a5aa.js";import{_ as p,c as m,a as n,w as a,e as t,r as o,o as d,d as s,b as e}from"./404.md.2150d20a.js";const w=JSON.parse(`{"title":"MBE - Max's Block Entity","description":"","frontmatter":{"title":"MBE - Max's Block Entity","category":"Techniques","mention":["BedrockCommands","zheaEvyline"],"tags":["method"]},"headers":[{"level":2,"title":"Introduction","slug":"introduction","link":"#introduction","children":[{"level":3,"title":"Points to Note","slug":"points-to-note","link":"#points-to-note","children":[]}]},{"level":2,"title":"Video Demonstration","slug":"video-demonstration","link":"#video-demonstration","children":[]},{"level":2,"title":"Setup","slug":"setup","link":"#setup","children":[]},{"level":2,"title":"System","slug":"system","link":"#system","children":[{"level":3,"title":"Purpose Of Each Command","slug":"purpose-of-each-command","link":"#purpose-of-each-command","children":[]}]},{"level":2,"title":"Rotations & Alignments","slug":"rotations-alignments","link":"#rotations-alignments","children":[]},{"level":2,"title":"Saving & Loading MBE","slug":"saving-loading-mbe","link":"#saving-loading-mbe","children":[]},{"level":2,"title":"MBE Tool","slug":"mbe-tool","link":"#mbe-tool","children":[]},{"level":2,"title":"Creator Socials","slug":"creator-socials","link":"#creator-socials","children":[]}],"relativePath":"commands/block-entities.md"}`),u={name:"commands/block-entities.md"},b=t("",12),h=t("",6),y=e("div",{class:"language-yaml line-numbers-mode"},[e("button",{title:"Copy Code",class:"copy"}),e("span",{class:"lang"},"yaml"),e("pre",{class:"shiki"},[e("code",null,[e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face North")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~-1.1245 ~0.2260 ~-0.097 ~81")]),s(`
+`),e("span",{class:"line"}),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face South")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~1.1245 ~0.2260 ~0.097 ~260")]),s(`
+`),e("span",{class:"line"}),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face East")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~0.097 ~0.2260 ~-1.1245 ~171")]),s(`
+`),e("span",{class:"line"}),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face West")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~-0.097 ~0.2260 ~1.1245 ~350")]),s(`
+`),e("span",{class:"line"})])]),e("div",{class:"line-numbers-wrapper"},[e("span",{class:"line-number"},"1"),e("br"),e("span",{class:"line-number"},"2"),e("br"),e("span",{class:"line-number"},"3"),e("br"),e("span",{class:"line-number"},"4"),e("br"),e("span",{class:"line-number"},"5"),e("br"),e("span",{class:"line-number"},"6"),e("br"),e("span",{class:"line-number"},"7"),e("br"),e("span",{class:"line-number"},"8"),e("br"),e("span",{class:"line-number"},"9"),e("br"),e("span",{class:"line-number"},"10"),e("br"),e("span",{class:"line-number"},"11"),e("br")])],-1),_=e("div",{class:"language-yaml line-numbers-mode"},[e("button",{title:"Copy Code",class:"copy"}),e("span",{class:"lang"},"yaml"),e("pre",{class:"shiki"},[e("code",null,[e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face North")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~-0.417~-0.5 ~-0.035 ~81")]),s(`
+`),e("span",{class:"line"}),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face South")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~0.417 ~-0.5 ~0.035 ~260")]),s(`
+`),e("span",{class:"line"}),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face East")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~0.035 ~-0.5 ~-0.417 ~171")]),s(`
+`),e("span",{class:"line"}),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face West")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~-0.035 ~-0.5 ~0.417 ~350")]),s(`
+`),e("span",{class:"line"})])]),e("div",{class:"line-numbers-wrapper"},[e("span",{class:"line-number"},"1"),e("br"),e("span",{class:"line-number"},"2"),e("br"),e("span",{class:"line-number"},"3"),e("br"),e("span",{class:"line-number"},"4"),e("br"),e("span",{class:"line-number"},"5"),e("br"),e("span",{class:"line-number"},"6"),e("br"),e("span",{class:"line-number"},"7"),e("br"),e("span",{class:"line-number"},"8"),e("br"),e("span",{class:"line-number"},"9"),e("br"),e("span",{class:"line-number"},"10"),e("br"),e("span",{class:"line-number"},"11"),e("br")])],-1),g=e("div",{class:"language-yaml line-numbers-mode"},[e("button",{title:"Copy Code",class:"copy"}),e("span",{class:"lang"},"yaml"),e("pre",{class:"shiki"},[e("code",null,[e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face North")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~-0.097 ~0.2325 ~1.1245 ~350")]),s(`
+`),e("span",{class:"line"}),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face South")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~0.097 ~0.2325 ~-1.1245 ~171")]),s(`
+`),e("span",{class:"line"}),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face East")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~-1.1245 ~0.2325 ~-0.097 ~81")]),s(`
+`),e("span",{class:"line"}),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face West")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~1.1245 ~0.2325 ~0.097 ~260")]),s(`
+`),e("span",{class:"line"})])]),e("div",{class:"line-numbers-wrapper"},[e("span",{class:"line-number"},"1"),e("br"),e("span",{class:"line-number"},"2"),e("br"),e("span",{class:"line-number"},"3"),e("br"),e("span",{class:"line-number"},"4"),e("br"),e("span",{class:"line-number"},"5"),e("br"),e("span",{class:"line-number"},"6"),e("br"),e("span",{class:"line-number"},"7"),e("br"),e("span",{class:"line-number"},"8"),e("br"),e("span",{class:"line-number"},"9"),e("br"),e("span",{class:"line-number"},"10"),e("br"),e("span",{class:"line-number"},"11"),e("br")])],-1),E=e("div",{class:"language-yaml line-numbers-mode"},[e("button",{title:"Copy Code",class:"copy"}),e("span",{class:"lang"},"yaml"),e("pre",{class:"shiki"},[e("code",null,[e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face North")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~-0.097 ~0.2325 ~1.1245 ~350")]),s(`
+`),e("span",{class:"line"}),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face South")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~0.097 ~0.2325 ~-1.1245 ~171")]),s(`
+`),e("span",{class:"line"}),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face East")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~-1.1245 ~0.2325 ~-0.097 ~81")]),s(`
+`),e("span",{class:"line"}),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face West")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~1.1245 ~0.2325 ~0.097 ~260")]),s(`
+`),e("span",{class:"line"})])]),e("div",{class:"line-numbers-wrapper"},[e("span",{class:"line-number"},"1"),e("br"),e("span",{class:"line-number"},"2"),e("br"),e("span",{class:"line-number"},"3"),e("br"),e("span",{class:"line-number"},"4"),e("br"),e("span",{class:"line-number"},"5"),e("br"),e("span",{class:"line-number"},"6"),e("br"),e("span",{class:"line-number"},"7"),e("br"),e("span",{class:"line-number"},"8"),e("br"),e("span",{class:"line-number"},"9"),e("br"),e("span",{class:"line-number"},"10"),e("br"),e("span",{class:"line-number"},"11"),e("br")])],-1),C=e("div",{class:"language-yaml line-numbers-mode"},[e("button",{title:"Copy Code",class:"copy"}),e("span",{class:"lang"},"yaml"),e("pre",{class:"shiki"},[e("code",null,[e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face North")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~-1.1245 ~0.484 ~-0.097 ~81")]),s(`
+`),e("span",{class:"line"}),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face South")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~1.1245 ~0.484 ~0.097 ~260")]),s(`
+`),e("span",{class:"line"}),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face East")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~0.097 ~0.484 ~-1.1245 ~171")]),s(`
+`),e("span",{class:"line"}),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#676E95"}},"# Face West")]),s(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#C3E88D"}},"tp @e [type=armor_stand, name=Grumm, c=1] ~-0.097 ~0.484 ~1.1245 ~350")]),s(`
+`),e("span",{class:"line"})])]),e("div",{class:"line-numbers-wrapper"},[e("span",{class:"line-number"},"1"),e("br"),e("span",{class:"line-number"},"2"),e("br"),e("span",{class:"line-number"},"3"),e("br"),e("span",{class:"line-number"},"4"),e("br"),e("span",{class:"line-number"},"5"),e("br"),e("span",{class:"line-number"},"6"),e("br"),e("span",{class:"line-number"},"7"),e("br"),e("span",{class:"line-number"},"8"),e("br"),e("span",{class:"line-number"},"9"),e("br"),e("span",{class:"line-number"},"10"),e("br"),e("span",{class:"line-number"},"11"),e("br")])],-1),k=t("",5),f=t("",3);function v(T,D,S,B,x,G){const r=o("CodeHeader"),l=o("Spoiler"),i=o("CardLink");return d(),m("div",null,[b,n(r,null,{default:a(()=>[s("mcfunction")]),_:1}),h,n(l,{title:"Full MBE"},{default:a(()=>[y]),_:1}),n(l,{title:"Mini MBE"},{default:a(()=>[_]),_:1}),n(l,{title:"Stair MBE"},{default:a(()=>[g]),_:1}),n(l,{title:"Bottom Slab MBE"},{default:a(()=>[E]),_:1}),n(l,{title:"Top Slab MBE"},{default:a(()=>[C]),_:1}),k,n(i,{imgsrcLight:"assets/images/homepage/BClogo.png",title:"Download File",link:"https://google.com"}),f])}const N=p(u,[["render",v]]);export{w as __pageData,N as default};
